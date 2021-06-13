bc4bf1715f441ed59bd2d0eb262acec9f54fd2cf
nils <nils@mediastruktur.se>
2021-06-13 21:57:19 +0200

    Tried out return values etc

--- src/gluonPretrainedSsdTutorial.py
+++ src/gluonPretrainedSsdTutorial.py
@@ -1,18 +1,20 @@
 from gluoncv import model_zoo, data, utils
+from mxnet.ndarray.ndarray import NDArray
 from matplotlib import pyplot as plt
+import numpy
 
-def run(fname):
+def run(imageArray):
     print('SSD')
+    # Need a way to keep this model locally, it now downloads every run
+    
     net = model_zoo.get_model('ssd_512_resnet50_v1_voc', pretrained=True)
 
     # im_fname = utils.download('https://github.com/dmlc/web-data/blob/master/gluoncv/detection/street_small.jpg?raw=true',
     #                          path='street_small.jpg')
 
-    x, img = data.transforms.presets.ssd.load_test(fname, short=512)
+    x, img = data.transforms.presets.ssd.transform_test(imageArray, short=512)
     print('Shape of pre-processed image:', x.shape)
 
     class_IDs, scores, bounding_boxes = net(x)
 
-    ax = utils.viz.plot_bbox(img, bounding_boxes[0], scores[0],
-                            class_IDs[0], class_names=net.classes)
-    plt.show()
+    return class_IDs, scores, bounding_boxes, net.classes, img
--- src/manage.py
+++ src/manage.py
@@ -1,12 +1,23 @@
 from gluonPretrainedFasterRcnn import run as runRcnn
 from gluonPretrainedSsdTutorial import run as runSsd
 from gluonPretrainedYolo import run as runYolo
+import mxnet as mx
 import os
+from matplotlib import pyplot as plt
+from gluoncv import utils
 
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 rel_path = "../testdata/parkingoutside2-rotated.jpg"
 abs_file_path = os.path.join(script_dir, rel_path)
 
+with open(abs_file_path, 'rb') as fp:
+    str_image = fp.read()
+
+image = mx.img.imdecode(str_image)
+
 # runRcnn(abs_file_path)
-runSsd(abs_file_path)
 # runYolo(abs_file_path)
+class_IDs, scores, bounding_boxes, net_class_names, transformed_image = runSsd(image)
+
+ax = utils.viz.plot_bbox(transformed_image, bounding_boxes[0], scores[0], class_IDs[0], class_names=net_class_names)
+plt.show()
